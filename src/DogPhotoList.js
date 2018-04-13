3281de322bc0ebd7a8f1a46c51a4b038f875e013
Matt Lane <lane.matthew@gmail.com>
2018-04-13 14:54:52 -0700

    starting a new dog app

---
+++ src/DogPhotoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DogPhotoList extends Component {
+  render() {
+    return (
+      <div className="DogPhotoList">
+        <h1>HERE ARE SOME AWESOME DOGS</h1>
+      </div>
+    );
+  }
+}
+
+export default DogPhotoList;
