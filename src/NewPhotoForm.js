6a30faeee86cbf227c0ce5de270b4a62e54287f4
Matt Lane <lane.matthew@gmail.com>
2018-04-13 15:34:00 -0700

    making controlled component

--- src/NewPhotoForm.js
+++ src/NewPhotoForm.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
 
 class NewPhotoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      src: "",
+      caption: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    this.props.handleAdd({
-      src: e.target.src.value,
-      caption: e.target.caption.value
-    });
+    this.props.handleAdd({ ...this.state });
+    this.setState({ src: "", caption: "" });
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <input name="src" />
-        <input name="caption" />
+      <form onSubmit={this.handleSubmit}>
+        <input name="src" onChange={this.handleChange} value={this.state.src} />
+        <input
+          name="caption"
+          onChange={this.handleChange}
+          value={this.state.caption}
+        />
         <input type="submit" />
       </form>
     );
